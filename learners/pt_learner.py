d502895ab2d23d55932fa1a2bbfcec3551e2f510
M.Hassan Mojab <mh_mojab75@yahoo.com>
2021-10-24 20:32:22 +0330

    concat data and feed to model once

--- learners/pt_learner.py
+++ learners/pt_learner.py
@@ -31,40 +31,66 @@ def pt_learner(model, queue, criterion, optim, args, device):
     model.train()
     optim.zero_grad()
 
-    support_features = []
-    support_labels = []
+    queue_len = len(queue)
 
-    query_outputs = []
-    query_features = []
-    query_labels = []
+    data = {
+        "input_ids": torch.cat(
+            [item["support"]["input_ids"] for item in queue]
+            + [item["query"]["input_ids"] for item in queue]
+        ),
+        "attention_mask": torch.cat(
+            [item["support"]["attention_mask"] for item in queue]
+            + [item["query"]["attention_mask"] for item in queue]
+        ),
+    }
+    labels = torch.cat(
+        [item["support"]["label"] for item in queue]
+        + [item["query"]["label"] for item in queue]
+    ).to(device)
 
-    support_data_list = [item["support"] for item in queue]
-    query_data_list = [item["query"] for item in queue]
-    torch.cat
+    outputs, features = model.forward(data)
 
-    for item in queue:
-        support_data = item["support"]
-        query_data = item["query"]
+    prototypes = compute_prototypes(
+        features[: len(queue_len * args.shot)], labels[: len(queue_len * args.shot)]
+    )
 
-        _, features = model.forward(support_data, classify=False)
-        support_features.append(features)
+    loss = criterion(
+        features[len(queue_len * args.shot) :],
+        outputs[len(queue_len * args.shot) :],
+        labels[len(queue_len * args.shot) :],
+        prototypes,
+    )
 
-        outputs, features = model.forward(query_data)
-        query_outputs.append(outputs)
-        query_features.append(features)
+    # support_features = []
+    # support_labels = []
 
-        support_labels.append(support_data["label"])
-        query_labels.append(query_data["label"])
+    # query_outputs = []
+    # query_features = []
+    # query_labels = []
 
-    support_features = torch.cat(support_features)
-    support_labels = torch.cat(support_labels).to(device)
-    prototypes = compute_prototypes(support_features, support_labels)
+    # for item in queue:
+    #     support_data = item["support"]
+    #     query_data = item["query"]
 
-    query_outputs = torch.cat(query_outputs)
-    query_features = torch.cat(query_features)
-    query_labels = torch.cat(query_labels).to(device)
+    #     _, features = model.forward(support_data, classify=False)
+    #     support_features.append(features)
 
-    loss = criterion(query_features, query_outputs, query_labels, prototypes)
+    #     outputs, features = model.forward(query_data)
+    #     query_outputs.append(outputs)
+    #     query_features.append(features)
+
+    #     support_labels.append(support_data["label"])
+    #     query_labels.append(query_data["label"])
+
+    # support_features = torch.cat(support_features)
+    # support_labels = torch.cat(support_labels).to(device)
+    # prototypes = compute_prototypes(support_features, support_labels)
+
+    # query_outputs = torch.cat(query_outputs)
+    # query_features = torch.cat(query_features)
+    # query_labels = torch.cat(query_labels).to(device)
+
+    # loss = criterion(query_features, query_outputs, query_labels, prototypes)
     loss.backward()
     torch.nn.utils.clip_grad_norm_(model.parameters(), args.grad_clip)
     optim.step()
